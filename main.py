e8e48e95f21a1132a2bb81c2aa230d80218aefa1
David Lozzi <david@lozzi.net>
2023-12-14 20:57:54 -0500

    a touch of error handling

--- main.py
+++ main.py
@@ -81,56 +81,70 @@ def all_is_bright(payload: dict):
 
     queue = '<?xml version="1.0"?><PlayList><ListName>iHeartRadio</ListName><ListInfo><Radio>1</Radio><SourceName>iHeartRadio</SourceName><PicUrl></PicUrl><TrackNumber>1</TrackNumber><SearchUrl></SearchUrl><Quality>0</Quality></ListInfo><Tracks><Track1><URL>http://stream.revma.ihrhls.com/zc4596</URL><Source>iHeartRadio</Source><Key></Key><Id></Id><Metadata>&amp;lt;?xml version=&amp;quot;1.0&amp;quot; encoding=&amp;quot;UTF-8&amp;quot;?&amp;gt;&amp;lt;DIDL-Lite xmlns:dc=&amp;quot;http://purl.org/dc/elements/1.1/&amp;quot; xmlns:upnp=&amp;quot;urn:schemas-upnp-org:metadata-1-0/upnp/&amp;quot; xmlns:song=&amp;quot;www.wiimu.com/song/&amp;quot; xmlns:custom=&amp;quot;www.wiimu.com/custom/&amp;quot; xmlns=&amp;quot;urn:schemas-upnp-org:metadata-1-0/DIDL-Lite/&amp;quot;&amp;gt;&amp;lt;upnp:class&amp;gt;object.item.audioItem.musicTrack&amp;lt;/upnp:class&amp;gt;&amp;lt;item&amp;gt;&amp;lt;song:id&amp;gt;&amp;lt;/song:id&amp;gt;&amp;lt;song:albumid&amp;gt;&amp;lt;/song:albumid&amp;gt;&amp;lt;song:singerid&amp;gt;&amp;lt;/song:singerid&amp;gt;&amp;lt;dc:title&amp;gt;iHeart Christmas&amp;lt;/dc:title&amp;gt;&amp;lt;upnp:artist&amp;gt;iHeartRadio&amp;lt;/upnp:artist&amp;gt;&amp;lt;upnp:album&amp;gt;iHeartRadio&amp;lt;/upnp:album&amp;gt;&amp;lt;res&amp;gt;http://stream.revma.ihrhls.com/zc4596&amp;lt;/res&amp;gt;&amp;lt;upnp:albumArtURI&amp;gt;https://i.iheart.com/v3/re/new_assets/b85767b2-d530-468e-8979-d0aa0319b7c8&amp;lt;/upnp:albumArtURI&amp;gt;&amp;lt;/item&amp;gt;&amp;lt;/DIDL-Lite&amp;gt;</Metadata></Track1></Tracks></PlayList>'
 
-    if intent == "LowerVolumeIntent":
-        print("Getting volume")
-        current_volume = rendering_ctrl.GetVolume(InstanceID=0, Channel="Master")
-        print(f"Current vol: {current_volume['CurrentVolume']}")
-        new_volume = 0
-        if volume > 0 and amount == 0:
-            new_volume = volume
-        elif volume == 0 and amount > 0:
-            new_volume = current_volume["CurrentVolume"] - amount
+    try:
+        if intent == "LowerVolumeIntent":
+            print("Getting volume")
+            current_volume = rendering_ctrl.GetVolume(InstanceID=0, Channel="Master")
+            print(f"Current vol: {current_volume['CurrentVolume']}")
+            new_volume = 0
+            if volume > 0 and amount == 0:
+                new_volume = volume
+            elif volume == 0 and amount > 0:
+                new_volume = current_volume["CurrentVolume"] - amount
 
-        print(f"Setting volume to {new_volume}")
-        rendering_ctrl.SetVolume(
-            InstanceID=0,
-            Channel="Master",
-            DesiredVolume=new_volume,
-        )
-        return_message = f"Volume set to {new_volume}"
-    elif intent == "PauseMusic":
-        av_transport.Pause(InstanceID=0)
-        print("Paused music")
-        return_message = "Music paused"
-    elif intent == "PlayMusic":
-        play_queue.CreateQueue(QueueContext=queue)
-        play_queue.PlayQueueWithIndex(QueueName="iHeartRadio", Index=1)
-        print("Play music")
-        return_message = "Playing music"
-    else:
-        print("Turning box on")
-        send_command_to_box("FFAA MODE START BB", ip)
+            print(f"Setting volume to {new_volume}")
+            rendering_ctrl.SetVolume(
+                InstanceID=0,
+                Channel="Master",
+                DesiredVolume=new_volume,
+            )
+            return_message = f"Volume set to {new_volume}"
+        elif intent == "PauseMusic":
+            av_transport.Pause(InstanceID=0)
+            print("Paused music")
+            return_message = "Music paused"
+        elif intent == "PlayMusic":
+            play_queue.CreateQueue(QueueContext=queue)
+            play_queue.PlayQueueWithIndex(QueueName="iHeartRadio", Index=1)
+            print("Play music")
+            return_message = "Playing music"
+        else:
+            print("Turning box on")
+            send_command_to_box("FFAA MODE START BB", ip)
 
-        print("Playing music")
-        play_queue.CreateQueue(QueueContext=queue)
-        play_queue.PlayQueueWithIndex(QueueName="iHeartRadio", Index=1)
+            print("Playing music")
+            play_queue.CreateQueue(QueueContext=queue)
+            play_queue.PlayQueueWithIndex(QueueName="iHeartRadio", Index=1)
 
-        print("Setting volume")
-        rendering_ctrl.SetVolume(InstanceID=0, Channel="Master", DesiredVolume=70)
+            print("Setting volume")
+            rendering_ctrl.SetVolume(InstanceID=0, Channel="Master", DesiredVolume=70)
 
-        print("Changing function to 10")
-        send_command_to_box("FFAA MODE CHOICE 10 BB", ip)
+            print("Changing function to 10")
+            send_command_to_box("FFAA MODE CHOICE 10 BB", ip)
 
-        print("All done, merry christmas!")
+            print("All done, merry christmas!")
 
-    return {
-        "version": "1.0",
-        "response": {
-            "outputSpeech": {
-                "type": "PlainText",
-                "text": return_message,
-                "playBehavior": "REPLACE_ENQUEUED",
+        return {
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": return_message,
+                    "playBehavior": "REPLACE_ENQUEUED",
+                },
+                "shouldEndSession": True,
             },
-            "shouldEndSession": True,
-        },
-    }
+        }
+    except Exception as e:
+        print(e)
+        return {
+            "version": "1.0",
+            "response": {
+                "outputSpeech": {
+                    "type": "PlainText",
+                    "text": "He's a south pole elf.",
+                    "playBehavior": "REPLACE_ENQUEUED",
+                },
+                "shouldEndSession": True,
+            },
+        }
